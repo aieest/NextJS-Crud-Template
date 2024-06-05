2af272d69cbe3b01fb24b093197db639dbe56a46
aieest <aie.estidola@gmail.com>
2024-06-06 11:45:43 +1200

    Installed heroicons v1

---
+++ src/heroicons.d.ts
@@ -0,0 +1,8 @@
+declare module '@heroicons/react/solid' {
+    import { ComponentType, SVGProps } from 'react';
+    export const PencilAltIcon: ComponentType<SVGProps<SVGSVGElement>>;
+    export const TrashIcon: ComponentType<SVGProps<SVGSVGElement>>;
+    export const HomeIcon: ComponentType<SVGProps<SVGSVGElement>>;
+    export const EyeIcon: ComponentType<SVGProps<SVGSVGElement>>;
+    // Add more icons as needed
+  }
