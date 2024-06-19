dedd3603901869d4b9397792c87df99f8dcdbbab
aieest <aie.estidola@gmail.com>
2024-06-19 14:36:33 +1200

    Wraped edit and create in require-auth

--- src/app/posts/[id]/edit/page.tsx
+++ src/app/posts/[id]/edit/page.tsx
@@ -1,26 +1,24 @@
 import { updatePost } from "@/app/actions/posts";
 import PostForm from "@/components/post-form";
 import { fetchPostById } from "@/db/queries/posts";
+import RequireAuth from "@/components/require-auth";
 
 interface PostsEditProps {
-    params: {
-        id: string;
-    };
+  params: {
+    id: string;
+  };
 }
 
 export default async function PostsEdit({ params }: PostsEditProps) {
+  const { id } = params;
+  const post = await fetchPostById(id);
+  const updateAction = updatePost.bind(null, id);
 
-    const { id } = params;
-
-    const post = await fetchPostById(id)
-
-    const updateAction = updatePost.bind(null, id)
-
-    return (
-        <>
-            <div className="post-form-div text-center p-4 shadow-md rounded-lg">
-                <PostForm formAction={updateAction} initialData={{ title: post?.title ?? '', content: post?.content ?? '' }} />
-            </div>
-        </>
-    );
+  return (
+    <RequireAuth>
+      <div className="post-form-div text-center p-4 shadow-md rounded-lg">
+        <PostForm formAction={updateAction} initialData={{ title: post?.title ?? '', content: post?.content ?? '' }} />
+      </div>
+    </RequireAuth>
+  );
 }
--- src/app/posts/create/page.tsx
+++ src/app/posts/create/page.tsx
@@ -1,12 +1,13 @@
 import { createPost } from "@/app/actions/posts";
 import PostForm from "@/components/post-form";
+import RequireAuth from "@/components/require-auth";
 
 export default function PostsCreate() {
-    return (
-        <>
-            <div className="post-form-div text-center p-6 shadow-md rounded-lg">
-                <PostForm formAction={createPost} initialData={{ title: '', content: '' }} />
-            </div>
-        </>
-    );
+  return (
+    <RequireAuth>
+      <div className="post-form-div text-center p-6 shadow-md rounded-lg">
+        <PostForm formAction={createPost} initialData={{ title: '', content: '' }} />
+      </div>
+    </RequireAuth>
+  );
 }
